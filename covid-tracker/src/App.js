8779c716ed5d22c399a5e4715f2d6eb21c36f6fa
JC <jctrilamanila@gmail.com>
2021-03-06 05:33:47 -0500

    made infoboxes responsive to onclick events

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -92,6 +92,8 @@ function App() {
         </div>
         <div className="app__stats">
           <InfoBox
+            isRed
+            active={casesType === "cases"}
             onClick={e => setCasesType("cases")}
             title="Coronavirus Cases"
             cases={prettyPrintStat(countryInfo.todayCases)}
@@ -99,6 +101,7 @@ function App() {
           />
 
           <InfoBox
+            active={casesType === "recovered"}
             onClick={e => setCasesType("recovered")}
             title="Recovered"
             cases={prettyPrintStat(countryInfo.todayRecovered)}
@@ -106,6 +109,8 @@ function App() {
           />
 
           <InfoBox
+            isRed
+            active={casesType === "deaths"}
             onClick={e => setCasesType("deaths")}
             title="Deaths"
             cases={prettyPrintStat(countryInfo.todayDeaths)}
