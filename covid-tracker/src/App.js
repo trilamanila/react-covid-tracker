2b110d8f5b47761e1f78ad86b6169dd479c6032d
JC <jctrilamanila@gmail.com>
2021-03-06 06:25:15 -0500

    styled graph

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -128,8 +128,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
-          <h3>Worldwide New {casesType}</h3>
-          <LineGraph casesType={casesType} />
+          <h3 className="app__graphTitle">Worldwide New {casesType}</h3>
+          <LineGraph className="app__graph" casesType={casesType} />
         </CardContent>
       </Card>
     </div>
